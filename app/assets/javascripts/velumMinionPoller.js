dbf18dc77daad2dc37717e88c9106cc341701dc1
Santiago Petrone <santiago.petrone@suse.com>
2017-03-06 11:52:52 +0100

    fix PR comments

--- app/assets/javascripts/velumMinionPoller.js
+++ app/assets/javascripts/velumMinionPoller.js
@@ -1,123 +1,102 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
+// jQuery plugin boilerplate used: jquery.basic.plugin-boilerplate.js
+// @see https://github.com/jquery-boilerplate/jquery-patterns
 ;( function( $, window, document, undefined ) {
 
-    "use strict";
+    'use strict';
 
-        // undefined is used here as the undefined global variable in ECMAScript 3 is
-        // mutable (ie. it can be changed by someone else). undefined isn't really being
-        // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-        // can no longer be modified.
+    // Create the defaults once
+    var pluginName = 'velumMinionPoller',
+        defaults = {
+            timeoutInterval: 5000,
+            nodesUrl: '/nodes'
+        };
 
-        // window and document are passed through as local variables rather than global
-        // as this (slightly) quickens the resolution process and can be more efficiently
-        // minified (especially when both are regularly referenced in your plugin).
+    // The actual plugin constructor
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
 
-        // Create the defaults once
-        var pluginName = "velumMinionPoller",
-            defaults = {
-                timeoutInterval: 5000,
-                nodesUrl: "/nodes"
-            };
+    // Avoid Plugin.prototype conflicts
+    $.extend( Plugin.prototype, {
+        init: function() {
+            this.pollingInterval = null;
+            this.pollNodes();
+        },
 
-        // The actual plugin constructor
-        function Plugin ( element, options ) {
-            this.element = element;
+        pollNodes: function () {
 
-            // jQuery has an extend method which merges the contents of two or
-            // more objects, storing the result in the first object. The first object
-            // is generally empty as we don't want to alter the default options for
-            // future instances of the plugin
-            this.settings = $.extend( {}, defaults, options );
-            this._defaults = defaults;
-            this._name = pluginName;
-            this.init();
+            var self = this;
+
+            self.pollingInterval = setInterval(
+                function () {
+                    var jqxhr = $.ajax({
+                        url: self.settings.nodesUrl,
+                        cache: false,
+                        context: self,
+                        dataType: 'json'
+                    }).then(
+                        //On success
+                        self.processNodes,
+                        // On failure
+                        self.handleError
+                    );
+                },
+                self.settings.timeoutInterval
+            );
+        },
+
+        stopPolling: function () {
+            clearInterval(this.pollingInterval);
+        },
+
+        processNodes: function (nodesResponse) {
+            var self = this;
+            $.each(nodesResponse, function (index, value) {
+                 self.renderNode(value);
+            });
+        },
+
+        renderNode: function (node) {
+
+            if ( $(this.element).has('#node-' + node.id).length ) {
+
+                return;
+
+            } else {
+                var nodeRow = [
+                    '<div id="node-' + node.id + '">',
+                        '<a href="/nodes/' + node.id + '">' + node.hostname + '</a>',
+                        '<dl>',
+                            '<dt>Created at</dt>',
+                            '<dd>' + node.created_at + '</dd>',
+                            '<dt>Updated at</dt>',
+                            '<dd>' + node.updated_at + '</dd>',
+                        '</dl>',
+                    '</div>'
+                ].join('');
+
+                $( this.element ).append( nodeRow );
+            }
+        },
+
+        handleError: function () {
+            // To be defined
         }
+    } );
 
-        // Avoid Plugin.prototype conflicts
-        $.extend( Plugin.prototype, {
-            init: function() {
-
-                // Place initialization logic here
-                // You already have access to the DOM element and
-                // the options via the instance, e.g. this.element
-                // and this.settings
-                // you can add more functions like the one below and
-                // call them like the example below
-
-                this.pollingInterval = null;
-
-                this.pollNodes();
-            },
-
-            pollNodes: function () {
-                var self = this;
-                self.pollingInterval = setInterval(
-                    function () {
-                        var jqxhr = $.ajax({
-                            url: self.settings.nodesUrl,
-                            cache: false,
-                            context: self,
-                            dataType: "json"
-                        }).then(
-                            //On success
-                            self.processNodes,
-                            // On failure
-                            self.handleError
-                        );
-                    },
-                    self.settings.timeoutInterval
-                );
-            },
-
-            stopPolling: function () {
-                clearInterval(this.pollingInterval);
-            },
-
-            processNodes: function (nodesResponse) {
-                var self = this;
-                $.each(nodesResponse, function (index, value) {
-                     self.renderNode(value);
-                });
-            },
-
-            renderNode: function (node) {
-
-                if ( $(this.element).has('#node-' + node.id).length ) {
-
-                    return;
-
-                } else {
-                    var nodeRow = [
-                        '<div id="node-' + node.id + '">',
-                            '<a href="/nodes/' + node.id + '">' + node.hostname + '</a>',
-                            '<dl>',
-                                '<dt>Created at</dt>',
-                                '<dd>' + node.created_at + '</dd>',
-                                '<dt>Updated at</dt>',
-                                '<dd>' + node.updated_at + '</dd>',
-                            '</dl>',
-                        '</div>'
-                    ].join('');
-
-                    $( this.element ).append( nodeRow );
-                }
-            },
-
-            handleError: function () {
-                // To be defined
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[ pluginName ] = function( options ) {
+        return this.each( function() {
+            if ( !$.data( this, 'plugin_' + pluginName ) ) {
+                $.data( this, 'plugin_' +
+                    pluginName, new Plugin( this, options ) );
             }
         } );
-
-        // A really lightweight plugin wrapper around the constructor,
-        // preventing against multiple instantiations
-        $.fn[ pluginName ] = function( options ) {
-            return this.each( function() {
-                if ( !$.data( this, "plugin_" + pluginName ) ) {
-                    $.data( this, "plugin_" +
-                        pluginName, new Plugin( this, options ) );
-                }
-            } );
-        };
+    };
 
 } )( jQuery, window, document );
